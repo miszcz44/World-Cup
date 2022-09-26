7926695bbd36a4ca0e9fb745daf308eb06f87e8f
Eryk Bojaruniec <erykb412@wp.pl>
2022-09-26 15:18:03 +0200

    Inicjacja

---
+++ src/test/java/pl/WorldCup/WorldCup/WorldCupApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.WorldCup.WorldCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
